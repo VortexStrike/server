32b201b48f51dc3a9a9e745172dcf37160a59b07
Vince Grassia <593223+vgrassia@users.noreply.github.com>
2025-07-16 15:15:44 -0400

    Add Health Controller stubs

---
+++ src/Billing/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Bit.Core.Utilities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bit.Billing.Controllers;
+
+public class HealthController : Controller
+{
+    [HttpGet("~/healthz")]
+    public IActionResult GetHealthZ()
+    {
+        // TODO: Implement logic for accurate health status of service.
+        return Ok(new { status = "healthy", timestamp = DateTime.UtcNow });
+    }
+}
